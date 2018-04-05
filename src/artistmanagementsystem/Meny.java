c40a3965d9cbbc58feb2df77a7844115bb388b8a
User <User@DESK-43QB8C2.lexicon.edu.local>
2018-04-05 09:03:09 +0200

    Commiting

---
+++ src/artistmanagementsystem/Meny.java
@@ -0,0 +1,26 @@
+
+package artistmanagementsystem;
+
+
+public class Meny {
+
+        public void Interface(){
+        System.out.println("Welcome to our Artist portal");
+        System.out.println("");
+        System.out.println("Please enter your choice below from following options");
+        System.out.println("");
+        System.out.println("1. View the current arist list");
+        System.out.println("");
+        System.out.println("2. Add an Artist");
+        System.out.println("");
+        System.out.println("3. Update Artist information");
+        System.out.println("");
+        System.out.println("4. Delete an Artist");
+        System.out.println("");
+        System.out.println("5. Show information about specfic employee by giving ID number");
+        System.out.println("");
+        System.out.println("6. Exit");
+        System.out.println("");
+        
+        }
+    }
